cd9cb9fd1fc93377bf293ff440f74e6ad15de196
WeeRG91 <126579703+WeeRG91@users.noreply.github.com>
2024-09-24 18:02:14 +0200

    Checkout page/Payment

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -18,12 +18,10 @@ export interface Config {
   };
   globals: {};
 }
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "users".
- */
 export interface User {
   id: string;
+  products?: (string | Product)[] | null;
+  product_files?: (string | ProductFile)[] | null;
   role: 'admin' | 'user';
   updatedAt: string;
   createdAt: string;
@@ -38,10 +36,6 @@ export interface User {
   lockUntil?: string | null;
   password: string | null;
 }
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "products".
- */
 export interface Product {
   id: string;
   user?: (string | null) | User;
@@ -52,7 +46,7 @@ export interface Product {
   product_files: string | ProductFile;
   approvedForSale?: ('pending' | 'approved' | 'denied') | null;
   priceId?: string | null;
-  stripId?: string | null;
+  stripeId?: string | null;
   images: {
     image: string | Media;
     id?: string | null;
@@ -60,10 +54,6 @@ export interface Product {
   updatedAt: string;
   createdAt: string;
 }
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "product_files".
- */
 export interface ProductFile {
   id: string;
   user?: (string | null) | User;
@@ -75,13 +65,7 @@ export interface ProductFile {
   filesize?: number | null;
   width?: number | null;
   height?: number | null;
-  focalX?: number | null;
-  focalY?: number | null;
 }
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "media".
- */
 export interface Media {
   id: string;
   user?: (string | null) | User;
@@ -93,8 +77,6 @@ export interface Media {
   filesize?: number | null;
   width?: number | null;
   height?: number | null;
-  focalX?: number | null;
-  focalY?: number | null;
   sizes?: {
     thumbnail?: {
       url?: string | null;
@@ -122,10 +104,6 @@ export interface Media {
     };
   };
 }
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "orders".
- */
 export interface Order {
   id: string;
   _isPaid: boolean;
@@ -134,10 +112,6 @@ export interface Order {
   updatedAt: string;
   createdAt: string;
 }
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "payload-preferences".
- */
 export interface PayloadPreference {
   id: string;
   user: {
@@ -157,10 +131,6 @@ export interface PayloadPreference {
   updatedAt: string;
   createdAt: string;
 }
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "payload-migrations".
- */
 export interface PayloadMigration {
   id: string;
   name?: string | null;
--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,8 +1,26 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import { ExpressContext } from "../server";
-
+import { PayloadRequest } from "payload/types";
+import { User } from "../payload-types";
 
 const trpc = initTRPC.context<ExpressContext>().create();
+const middleware = trpc.middleware;
+
+const isAuth = middleware(async ({ ctx, next }) => {
+  const req = ctx.req as PayloadRequest;
+  const { user } = req as { user: User | null };
+
+  if (!user || !user.id) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  return next({
+    ctx: {
+      user,
+    },
+  });
+});
 
 export const router = trpc.router;
 export const publicProcedure = trpc.procedure;
+export const privateProcedure = trpc.procedure.use(isAuth);
